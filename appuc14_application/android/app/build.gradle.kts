06f3788a0c2b502265fc1486f96b1c0f7650f758
carllos39 <140573083+carllos39@users.noreply.github.com>
2025-12-03 16:37:21 -0300

    atualizar

--- appuc14_application/android/app/build.gradle.kts
+++ appuc14_application/android/app/build.gradle.kts
@@ -1,15 +1,28 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
 
 android {
     namespace = "br.com.usuario.appuc14_application"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "br.com.usuario.appuc14_application"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -19,28 +32,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-
-    defaultConfig {
-        applicationId = "br.com.usuario.appuc14_application"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-
-        // Ativar multidex corretamente no Kotlin DSL
-        multiDexEnabled = true
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-}
-
-dependencies {
-    // Multidex necessário para Firebase (evita erro de métodos)
-    implementation("androidx.multidex:multidex:2.0.1")
 }
 
 flutter {
--- appuc14_application/android/build.gradle.kts
+++ appuc14_application/android/build.gradle.kts
@@ -4,12 +4,10 @@ buildscript {
         mavenCentral()
     }
 }
-
-plugins {
-    id("com.google.gms.google-services") version "4.4.4" apply false
-}
-
 tasks.register<Delete>("clean") {
     delete(layout.buildDirectory)
+}
+plugins {
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
