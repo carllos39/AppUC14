22e75ee3e6c5466484744a8f261f4d2075e98826
carllos39 <140573083+carllos39@users.noreply.github.com>
2025-12-04 21:21:06 -0300

    atualizar

--- appuc14_application/android/app/build.gradle.kts
+++ appuc14_application/android/app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 dependencies {
     // Multidex necessário para Firebase (evita erro de métodos)
     implementation("androidx.multidex:multidex:2.0.1")
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
 
 flutter {
--- appuc14_application/android/build.gradle.kts
+++ appuc14_application/android/build.gradle.kts
@@ -9,5 +9,6 @@ tasks.register<Delete>("clean") {
 }
 plugins {
     id("com.google.gms.google-services") version "4.4.4" apply false
+    
 }
 
